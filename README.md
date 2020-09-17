a023ff807117d9ffb51bb23eddbc434b08f0b6b2
oops <oops@localhost.localdomain>
2020-09-17 13:18:46 -0400

    files

--- README.md
+++ README.md
@@ -1,2 +1,64 @@
-# Minimal_Flask_Endpoint_API_for_Data_Science
-Minimal Flask Heroku Endpoint API for Deploying Data Science &amp; Machine Learning Functions
+#Minimal_Flask_Endpoint_API_for_Data_Science
+https://github.com/lineality/Minimal_Flask_Endpoint_API_for_Data_Science
+
+## Here is a guide for quickly setting up Minimal Flask Heroku Endpoint API for Deploying Data Science &amp; Machine Learning Functions, including files and tools for deploying and testing, including an HTML home page interface so you can test your functions directly from the page (and make that available to users if you want).
+
+## Steps
+- 1. (Optional) Make a github repo (optional sync heroku to that)
+- 2. Set up a file system and Pipenv environment for your project
+- 3. (Optional) Good idea to test out your basic endpoints locally
+- 4. Create a new app on heroku.com, follow instruction there (also here) to deploy (very much like github, but it's heroku-git. 
+- 5. (Optional) Test that it's working (tools here, and some built in). 
+- Done!
+
+# Guide 
+
+## Minimal Flask API for Data Science (Doc version of guide)
+https://docs.google.com/document/d/1BdTu-WxGwxuAQ_xVwjbnAXvGgQMQkYxGn_UGGV_4pPw/edit?usp=sharing
+
+##1. Files you Need
+- proc file
+- pipenv file
+- app.py
+	
+## 2. Running Flask locally to check
+- https://docs.google.com/document/d/1eMlHzh0mske6XF10rchp8qDbgAu9ii8Y1ygk_YMuRno/edit?usp=sharing
+
+- create pipenv environment (code in rReadMe)
+If you have a pipfile, you can try $ pipenv install
+which will re-create a pipenv/piplock from the pipfile
+
+- If you don't have a pipefile or it's out of date, or if for whatever reason you want to make a new fresh install, run the line below. You will probably add to this but it should work to start:
+```pipenv install certifi chardet click flask gunicorn idna itsdangerous jinja2 numpy pandas python-dateutil python-dotenv pytz requests urllib3 werkzeug flask-cors```
+
+- run/create pipenv shell ($ pipenv shell)
+
+Once you have a pipenv and shell set up and running:
+- $ python3 app.py
+
+## 3. Deploying to Heroku
+```
+# $ heroku login
+# $ cd my-project/
+# $ git init
+# $ heroku git:remote -a NAME_OF_YOUR_APP
+# $ git add .
+# $ git commit -am "make it better"
+# $ git push heroku master
+```
+
+## 4. Testing Endpoints
+- You can use postman locally and online, but here is a very quick colab that works for online (not locally).
+-https://colab.research.google.com/drive/1IluMraiJnpiMWwBhLo73czWTrvvqcAm_?usp=sharing
+
+
+
+
+### Heroku vs. AWS Notes:
+- app.route (heroku)
+- vs.
+- application.route (AWS)
+
+
+
+
---
+++ app.py
@@ -0,0 +1,151 @@
+#version 1: Miminal Endpoint API (Flask, Heroku)
+
+## Instructions For Heroku Login & Deploy
+#
+# $ heroku login
+# $ cd my-project/
+# $ git init
+# $ heroku git:remote -a NAME_OF_YOUR_APP
+# $ git add .
+# $ git commit -am "make it better"
+# $ git push heroku master
+
+## Instructions To create the pipenv in terminal
+# pipenv install certifi chardet click flask gunicorn idna itsdangerous jinja2 numpy pandas python-dateutil python-dotenv pytz requests urllib3 werkzeug flask-cors
+
+# ## sample pipenv
+# [[source]]
+# name = "pypi"
+# url = "https://pypi.org/simple"
+# verify_ssl = true
+#
+# [dev-packages]
+#
+# [packages]
+# certifi = "*"
+# chardet = "*"
+# click = "*"
+# flask = "*"
+# gunicorn = "*"
+# idna = "*"
+# itsdangerous = "*"
+# jinja2 = "*"
+# numpy = "*"
+# pandas = "*"
+# python-dateutil = "*"
+# python-dotenv = "*"
+# pytz = "*"
+# requests = "*"
+# urllib3 = "*"
+# werkzeug = "*"
+# flask-cors = "*"
+#
+# [requires]
+# python_version = "3.8"
+
+# Import Packages & Libraries
+from dotenv import load_dotenv
+from flask import Flask, jsonify, request, render_template
+from flask_cors import CORS
+
+# Optional
+dummy_data = {
+"A": 1,
+"B": "2",
+"C": 3.0,
+}
+
+# Required for Flask/Heroku
+app = Flask(__name__)
+CORS(app)
+
+# note: for herkoku or aws use private environment variables
+# use dotenv
+#
+# Need to install git secrets
+
+# Your Home Screen
+@app.route('/')
+def home():
+    # Displays a basic app written in HTML so that code can be tested directly.
+    return render_template('base.html')
+
+# Main Demo Endpoint, using user-input
+#
+# Likely you will eventually use this kind of endpoint
+# that runs a function, processes, input, and gives output
+
+
+def multiple_terms():
+    json_obj = {'search': [{'feature': request.values['feature1'], 'terms': request.values['terms1']},
+                            {'feature': request.values['feature2'], 'terms': request.values['terms2']}],
+                'offset': request.values['offset'], 'range': request.values['range']}
+
+@app.route('/test_demo_post', methods=['POST'])
+def test_demo_post():
+
+    # this gets the json object from the user
+    json_obj = {'input_1': request.values['input_1'], 'input_2': request.values['input_2']}
+
+    # variables that are use-able by python
+    input_1_item = json_obj["input_1"]
+    input_2_item = json_obj["input_2"]
+
+    ####
+    ## Your Actual function/model Code Goes Here
+    ####
+    # example of just returning the input
+    new_string = input_1_item + input_2_item
+    demo_output = {"answer": new_string}
+
+    # this turns output into a json object
+    json_output = jsonify(demo_output)
+
+    # returns the json format output
+    return json_output
+
+# input form: {'input_1': "x", 'input_2': "y"}
+@app.route('/demo_post', methods=['POST'])
+def demo_post():
+
+    # this gets the json object from the user
+    json_obj = request.get_json(force=True)
+
+    # These lines brake up the json object into
+    # variables that are use-able by python
+    input_1_item = json_obj["input_1"]
+    input_2_item = json_obj["input_2"]
+
+    ####
+    ## Your Actual function/model Code Goes Here
+    ####
+    # example of just returning the input
+    new_string = input_1_item + input_2_item
+    demo_output = {"answer": new_string}
+
+    # this turns output into a json object
+    json_output = jsonify(demo_output)
+
+    # returns the json format output
+    return json_output
+
+
+# dummy data is often useful at the start of a project before you have real data
+# no input needed for this endpoint
+@app.route('/dummy_data')
+def test_dummy_data():
+    return jsonify(dummy_data)
+
+
+# premade json files for output can be useful (super-fast)
+# no input needed for this endpoint
+@app.route('/prefab_1', methods=['GET'])
+def prefab_endpoint():
+    return open('json_file.json','r').read()
+
+
+if __name__ == "__main__":
+    # Setting debug to True enables debug output. This line should be
+    # removed before deploying a production app.
+    app.debug = True
+    app.run()
